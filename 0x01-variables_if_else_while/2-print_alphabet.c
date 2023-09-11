8aa383e215e2776865bcb0ee808a52e60568ae89
Celo90 <agbahowecomfort.com>
2023-09-11 16:39:20 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main -main function
+ *
+ * Return: always 0
+ */
+int main(void)
+{
+	char i
+	for (i = 'a' : 1 <= 'z' : 1++)
+		putchar(i);
+	putchar("\n");
+	return (0);
+}
