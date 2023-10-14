42d028ef85a25f0f7654c35abf8c4fed82e11856
Celo90 <agbahowecomfort.com>
2023-10-13 17:08:05 -0700

    0-print_name.c

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,10 +1,9 @@
 #include "function_pointers.h"
-#include <stdio.h>
+
 /**
  * print_name - print name using pointer to function
  * @name: string to add
- * @f: pointer to function
- * Return: nothing
+ * @f: variable to hold the string name
  **/
 void print_name(char *name, void (*f)(char *))
 	{
