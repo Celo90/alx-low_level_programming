aa3556b4c51871d6629a398cc22c6f3abbbe59b2
Celo90 <agbahowecomfort.com>
2023-10-29 17:31:31 -0700

    main.h

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * binary_to_unit - a program that converts a binary number to an unsigned int
+ * @b: a character pointing to a string containing binary numbers 0 and 1 chars
+ *
+ * Return: the converted number or 0
+ */
+
+unsigned int binary_to_uint(const char *b);
+{
+	unsigned int = (len, i, j, sum, pow);
+		int base;
+
+		base = 2:
+		sum = 0;
+		pow = 1;
+
+
