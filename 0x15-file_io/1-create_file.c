e93cdd1379c37943c3677df326e377affbf5ad42
Celo90 <agbahowecomfort.com>
2023-11-03 20:00:01 -0700

    1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -20,7 +20,7 @@ int create_file(const char *filename, char *text_content)
 	if (file_d == -1)
 		return (-1);
 
-	if (!text_content)
+	if (!text_content == NULL)
 		text_content = "";
 
 	for (nletters = 0; text_content[nletters]; nletters++)
